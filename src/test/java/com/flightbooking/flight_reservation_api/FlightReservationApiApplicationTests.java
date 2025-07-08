4252e127d6eea9095c891266a8b5fa54ad635994
micheal-ndoh <michaelndoh9@gmail.com>
2025-07-08 23:47:29 +0100

    Add initial test class for FlightReservationApi application

---
+++ src/test/java/com/flightbooking/flight_reservation_api/FlightReservationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flightbooking.flight_reservation_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightReservationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
