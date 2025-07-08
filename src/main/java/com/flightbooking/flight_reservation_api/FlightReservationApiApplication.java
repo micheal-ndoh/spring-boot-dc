d3bacf3664691b13d4b3c436c76e34fea5e85422
micheal-ndoh <michaelndoh9@gmail.com>
2025-07-08 23:47:37 +0100

    Add FlightReservationApiApplication class with main method

---
+++ src/main/java/com/flightbooking/flight_reservation_api/FlightReservationApiApplication.java
@@ -0,0 +1,13 @@
+package com.flightbooking.flight_reservation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservationApiApplication.class, args);
+	}
+
+}
