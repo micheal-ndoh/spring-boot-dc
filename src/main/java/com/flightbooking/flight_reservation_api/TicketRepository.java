c0458ed5fcaf9dbd865c1c1df2cb78d471b79a87
micheal-ndoh <michaelndoh9@gmail.com>
2025-07-09 08:08:56 +0100

    feat: add TicketRepository interface for ticket data access

---
+++ src/main/java/com/flightbooking/flight_reservation_api/TicketRepository.java
@@ -0,0 +1,13 @@
+package com.flightbooking.flight_reservation_api;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    List<Ticket> findByAddressContainingIgnoreCase(String address);
+    List<Ticket> findByDestinationAddressContainingIgnoreCase(String destinationAddress);
+    List<Ticket> findByKickoffAddressContainingIgnoreCase(String kickoffAddress);
+}
